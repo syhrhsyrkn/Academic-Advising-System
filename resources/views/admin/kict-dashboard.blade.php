f7fd7e7920d1e0b7c6946802d2e2dea4cb80aa7c
ArifPeycal <ariffaisal021123@gmail.com>
2024-11-28 11:51:05 +0800

    Remove jquery-3.6.0

--- resources/views/admin/kict-dashboard.blade.php
+++ resources/views/admin/kict-dashboard.blade.php
@@ -369,7 +369,6 @@
                 </div>
             </div>
         </div>
-        <script src="assets/js/jquery-3.6.0.min.js"></script>
         <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="assets/js/feather.min.js"></script>
         <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
