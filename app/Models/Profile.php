09a689131f475b1122a13c7bc5b0243fc763a5a3
syhrhsyrkn <syahirahsyarkan57@gmail.com>
2024-12-01 01:23:51 +0800

    sorry

---
+++ app/Models/Profile.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'full_name',
+        'contact_number',
+        'kulliyyah',
+        'department',
+        'specialisation',
+        'matric_no',
+        'year',
+        'semester',
+        'staff_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2024_11_30_115326_create_profiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    public function up()
+{
+    Schema::create('profiles', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('user_id');
+        $table->string('full_name');
+        $table->string('email'); 
+        $table->string('contact_number');
+        $table->string('kulliyyah');
+        $table->string('department');
+        $table->string('specialisation')->nullable(); // For students only
+        $table->string('matric_no')->nullable(); // For students only
+        $table->integer('year')->nullable(); // For students only
+        $table->integer('semester')->nullable(); // For students only
+        $table->string('staff_id')->nullable(); // For advisor/admin only
+        $table->timestamps();
+    });
+}
+
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
